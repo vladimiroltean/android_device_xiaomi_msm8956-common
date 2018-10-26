0aefbcbc89c539d2183ae95c4e691de76337c0dd
Vladimir Oltean <olteanv@gmail.com>
2018-10-26 04:43:40 +0300

    config.fs: import AID definitions from CAF device_qcom_common
    Change-Id: Ibd7bef780f4daab168240ba06f4b255f26c7b564
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- config.fs
+++ config.fs
@@ -1,3 +1,18 @@
+[AID_VENDOR_QTI_DIAG]
+value:2901
+
+[AID_VENDOR_QDSS]
+value:2902
+
+[AID_VENDOR_RFS]
+value:2903
+
+[AID_VENDOR_RFS_SHARED]
+value:2904
+
+[AID_VENDOR_ADPL_ODL]
+value:2905
+
 [system/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
